7d7f780f3bab4594a3d040c58c8ffb0e7af582e0
Sri Pavish S <sripavishsubbannan@gmail.com>
2026-08-08 22:54:03 +0530

    Add files via upload

---
+++ Sqrsort.java
@@ -0,0 +1,9 @@
+class Sqrsort {
+    public int[] sortedSquares(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            nums[i]=nums[i]*nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
